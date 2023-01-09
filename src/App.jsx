533d6999aeb296ff2ad7755347f09cd83bb3744c
jharikrichardson <jharik.rdson@gmail.com>
2023-01-09 15:22:39 -0500

    Added quiz state and conditiional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,34 @@ import Question from './components/Question'
 import testData from "./data/testData"
 import './App.css'
 
-function App() {
-  const [formData, setFormData] = useState(testData);
+const INITIAL_STATE = {
+  quizState: {
+    startQuiz: false,
+    checkAnswers: false
+  },
+  formData: testData,
+  score: 0
+}
 
-  function handleChange(event, id) {
+function App() {
+  const [quizState, setQuizState] = useState(INITIAL_STATE.quizState)
+  const [formData, setFormData] = useState(INITIAL_STATE.formData);
+  const [score, setScore] = useState(INITIAL_STATE.score);
+
+  function startQuiz(event) {
+    setQuizState(prevState => ({
+      ...prevState,
+      startQuiz: !prevState.startQuiz
+    }))
+  }
+
+  function resetQuiz(event) {
+    setFormData(INITIAL_STATE.formData)
+    setQuizState(INITIAL_STATE.quizState)
+    setScore(INITIAL_STATE.score)
+  }
+
+  function handleChange(event) {
     const { name, value } = event.target
     setFormData(oldData => {
       return oldData.map(question => {
@@ -19,14 +43,22 @@ function App() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    console.log(formData)
-    setFormData(testData)
+
+    const score = formData.reduce((acc, curr) => {
+      return curr.correctAnswer === curr.selectedAnswer ? acc + 1 : acc
+    }, 0)
+    setScore(score)
+    setQuizState(prevState => ({
+      ...prevState,
+      checkAnswers: !prevState.checkAnswers
+    }))
+    console.log("Running")
   }
 
   const answersElements = (answers, question, selected) => {
     return answers.map(answer => {
       return (
-        <label htmlFor="" key={nanoid()}>
+        <label htmlFor={answer} key={nanoid()}>
           <input
             type="radio"
             id={answer}
@@ -54,10 +86,23 @@ function App() {
 
   return (
     <main>
-      <form onSubmit={handleSubmit}>
-        {questions(formData)}
-        <button type="submit" className="start-game">Submit</button>
-      </form>
+      {
+        !quizState.startQuiz ?
+          <Welcome onClick={startQuiz} />
+          :
+          <form onSubmit={handleSubmit}>
+            {questions(formData)}
+            {
+              !quizState.checkAnswers ?
+                <button type="submit" className="start-game">Check Answers</button>
+                :
+                <div>
+                  <span>{score}</span>
+                  <button type="reset" className="start-game" onClick={resetQuiz}>Play Again</button>
+                </div>
+            }
+          </form>
+      }
     </main>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,9 @@
-function Welcome() {
+function Welcome({ onClick }) {
     return (
         <>
             <h1 className='game-title main-text'>Quizzical</h1>
             <p className='game-description'>Get ready for a fun and fast-paced custom trivia game! </p>
-            <button className='start-game'>Start quiz</button>
+            <button className='start-game' onClick={onClick}>Start quiz</button>
         </>
     )
 }
